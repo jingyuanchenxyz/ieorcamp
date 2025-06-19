95ac6744ea628662837ef209cd5870c27196ccd0
Forensic257 <jordanmatthewa.argel@gmail.com>
2025-06-19 11:12:55 -0700

    Added Cats And Dogs Datasets

---
+++ datasets/cats_and_dogs/training_set/cat/test.py
@@ -0,0 +1,13 @@
+#from tensorflow import keras
+#from tensorflow.keras import layers
+#from tensorflow.keras.datasets import imdb
+import numpy as np
+import os, shutil, pathlib
+#from keras.preprocessing.image import load_img
+#from keras.preprocessing.image import img_to_array
+#from keras.preprocessing.image import array_to_img
+import matplotlib.pyplot as plt
+#import tensorflow as tf
+from PIL import Image
+plt.figure(figsize=(10, 10))
+size_picture = 24
